a7a3b9664b6ebeb564e99957e1d37c3d919b750a
Aurelian Hermand <aurel@hermand.de>
2020-11-01 22:06:12 +0100

    responsive gear

--- resources/views/pages/jobs/index.blade.php
+++ resources/views/pages/jobs/index.blade.php
@@ -38,8 +38,7 @@
                                         <path fill-rule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 0 0 1.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0 0 14.828 8a13.133 13.133 0 0 0-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 0 0 1.172 8z"/>
                                         <path fill-rule="evenodd" d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
                                     </svg>
-                                    &nbsp;
-                                    View
+                                    <span class="d-none d-md-inline">&nbsp; View</span>
                                 </a>
                                 <a href="{{ route('jobs.edit', [app()->getLocale(), $job->id]) }}" class="btn-gear dark">
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-pencil-square" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -49,16 +48,14 @@
                                     <!-- <svg class="bi" width="32" height="32" fill="currentColor">
                                         <use xlink:href="/node_modules/bootstrap-icons/bootstrap-icons.svg#pencil-square"/>
                                     </svg> -->
-                                    &nbsp;
-                                    Edit
+                                    <span class="d-none d-md-inline">&nbsp; Edit</span>
                                 </a>
-                                <a href="{{ route('jobs.delete', [app()->getLocale(), $job->id]) }}" class="btn-gear dark">
+                                <a href="{{ route('jobs.delete', [app()->getLocale(), $job->id]) }}" class="btn-gear dark text-danger">
                                     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                                     </svg>
-                                    &nbsp;
-                                    Delete
+                                    <span class="d-none d-md-inline">&nbsp; Delete</span>
                                 </a>
                             </td>
                         </tr>
