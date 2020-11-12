32282e383676095d8d6fc4db5216d05aad72cf49
Aurelian Hermand <aurel@hermand.de>
2020-11-12 01:14:05 +0100

    📦 NEW: social media sharing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,4 +54,65 @@
     {{ $jobs->links() }}
 
 </div>
+
+
+<?php
+$social = (object)[
+    'title' => 'OpenJobPosting',
+    'url' => 'https://www.openjobposting.com',
+];
+?>
+<div id="socials">
+    <div class="inner">
+        <div class="bubbles">
+            <div class="inner">
+                <?php for($i = 1; $i <= 30; $i++) { echo '<div class="bubble bubble-'.$i.'"></div>'; } ?>
+            </div>
+        </div>
+        <div class="links">
+            <a rel="nofollow" href="https://www.facebook.com/sharer/sharer.php?u={{ $social->url }}&t={{ $social->title }}" target="_blank" title="Share on Facebook">
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    <use xlink:href="social-icons.svg#facebook"/>
+                </svg>
+            </a>
+            <a rel="nofollow" href="https://twitter.com/share?url={{ $social->url }}&text={{ $social->title }}" target="_blank" title="Share on Twitter">
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    <use xlink:href="social-icons.svg#twitter"/>
+                </svg>
+            </a>
+            <a rel="nofollow" href="whatsapp://send?text={{ $social->url }}" data-action="share/whatsapp/share" title="Share on Whatsapp">
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    <use xlink:href="social-icons.svg#whatsapp"/>
+                </svg>
+            </a>
+            <a rel="nofollow" href="https://www.linkedin.com/shareArticle?mini=true&url={{ $social->url }}&t={{ $social->title }}" target="_blank" title="Share on Linkedin">
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    <use xlink:href="social-icons.svg#linkedin"/>
+                </svg>
+            </a>
+            <a rel="nofollow" href="http://www.reddit.com/submit?url={{ $social->url }}" target="_blank" title="Share on Reddit">
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    <use xlink:href="social-icons.svg#reddit"/>
+                </svg>
+            </a>
+            <a rel="nofollow" href="mailto:?subject={{ $social->title }}&body={{ $social->url }}" title="Share on Mail">
+                <svg class="bi" width="32" height="32" fill="currentColor">
+                    <use xlink:href="social-icons.svg#mail"/>
+                </svg>
+            </a>
+        </div>
+    </div>
+</div>
+
+
+<div class="container">
+
+</div>
+@endsection
+
+
+@section('scripts')
+<style>
+
+</style>
 @endsection
